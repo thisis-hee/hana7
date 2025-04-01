03cd59e391d1d8c77cdc7fa343583e880d351ed2
thisis-hee <ish8176@dgu.ac.kr>
2025-04-01 10:19:03 +0900

    html

---
+++ html/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: "#da373d",
+                carrot: "#e67e22",
+                favorite: "#2ecc71"
+            }
+        }
+    }
+}
